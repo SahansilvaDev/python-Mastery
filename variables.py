016f63526ffb577888da761179208dd34f61edc8
Sahan Silva <sahansilva.main@gmail.com>
2025-01-13 23:40:41 +0530

    first commit

---
+++ variables.py
@@ -0,0 +1,20 @@
+#variables are used to store data values and name to refer to the data
+
+name = "kamal"
+
+age = 20
+
+
+weight = 56.2
+
+
+
+name,age,weight = "kamal",30,56.2
+
+mango = apple = banana = "fruit"
+
+mango = apple = banana = 50
+
+print(name)
+print(age)
+print(weight)
