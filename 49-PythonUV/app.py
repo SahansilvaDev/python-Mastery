09e3f9eb44a52e9909ca257a75e8bdda8070b205
Sahan Silva <sahansilva.main@gmail.com>
2026-02-22 23:10:36 +0530

    updated uv process

--- 49-PythonUV/app.py
+++ 49-PythonUV/app.py
@@ -9,7 +9,9 @@ import uv
 
 # 01 uv init sample_project
 
-# 02 uv venv
+
+# 02 uv venv - to create a virtual environment for the project, it will create a .venv folder in the project directory and it will also create a pyproject.toml file in the project directory to manage the dependencies of the project.
+# 02.1 .venv\Scripts\activate - to activate the virtual environment, it will change the current working directory to the .venv folder and it will also change the prompt to indicate that the virtual environment is active.
 
 
 # 03  uv run main.py
@@ -19,6 +21,9 @@ import uv
 # 05 if want -  uv tree # show the dependency tree of the project
 
 # 06 if want -  uv sync # to sync the dependencies of the project with the lockfile 
+
+# if want add requirements.txt - uv add -r requirements.txt # to add the dependencies of the project from the requirements.txt file, it will read the dependencies from the requirements.txt file and it will add them to the pyproject.toml file with their versions.
+# if want dependencies to requirements.txt file - uv export -r requirements.txt means to export the dependencies of the project to a requirements.txt file, it will create a requirements.txt file in the project directory and it will also add the dependencies of the project to the requirements.txt file with their versions.
 
 
 # Highlights
